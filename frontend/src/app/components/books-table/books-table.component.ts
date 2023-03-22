5b4bb13bb3c2bbed3201c30fd47ed6d5ba1a07bd
Robin Kukke <robkukke@users.noreply.github.com>
2023-03-23 00:29:37 +0200

    Support paging and sorting for library and checkout views

--- frontend/src/app/components/books-table/books-table.component.ts
+++ frontend/src/app/components/books-table/books-table.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { PageEvent } from '@angular/material/paginator';
 import { BookService } from '../../services/book.service';
 import { Observable } from 'rxjs';
-import { Page } from '../../models/page';
+import { Page, PageRequest } from '../../models/page';
 import { Book } from '../../models/book';
 
 @Component({
@@ -20,14 +22,23 @@ export class BooksTableComponent implements OnInit {
   // dataSource for mat-table to use.
   dataSource!: MatTableDataSource<any>;
 
+  // mat-paginator options.
+  pageSizeOptions = [10, 25, 50, 100];
+  showFirstLastButtons = true;
+
+  // PageRequest default options.
+  pageRequest: PageRequest = {
+    pageIndex: 0,
+    pageSize: 25
+  };
+
   constructor(
     private bookService: BookService,
   ) {
   }
 
   ngOnInit(): void {
-    // TODO this observable should emit books taking into consideration pagination, sorting and filtering options.
-    this.books$ = this.bookService.getBooks({});
+    this.books$ = this.bookService.getBooks(this.pageRequest);
 
     // Converts a page of books into a suitable array for mat-table to use as a dataSource.
     this.books$.subscribe(page => {
@@ -35,6 +46,26 @@ export class BooksTableComponent implements OnInit {
       this.dataSource = new MatTableDataSource(bookArray);
     });
 
+  }
+
+  // Handles the events of the user changing pageSize and pageIndex, then showing the updated mat-table.
+  handlePageEvent(event: PageEvent) {
+    this.pageRequest.pageIndex = event.pageIndex;
+    this.pageRequest.pageSize = event.pageSize;
+
+    this.books$ = this.bookService.getBooks(this.pageRequest);
+
+    this.books$.subscribe(page => this.dataSource.data = page.content);
+  }
+
+  // Sorts data based on the active column and sort direction.
+  sortData(event: any) {
+    this.pageRequest.sort = event.active;
+    this.pageRequest.direction = event.direction;
+
+    this.books$ = this.bookService.getBooks(this.pageRequest);
+
+    this.books$.subscribe(page => this.dataSource.data = page.content);
   }
 
 }
--- frontend/src/app/components/checkouts-table/checkouts-table.component.ts
+++ frontend/src/app/components/checkouts-table/checkouts-table.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { PageEvent } from '@angular/material/paginator';
 import { CheckoutService } from '../../services/checkout.service';
 import { Observable } from 'rxjs';
-import { Page } from '../../models/page';
+import { Page, PageRequest } from '../../models/page';
 import { Checkout } from '../../models/checkout';
 
 // RegEx function to separate words in the heading, while capitalizing the first letter of the first word in the heading.
@@ -33,13 +35,22 @@ export class CheckoutsTableComponent implements OnInit {
   // dataSource for mat-table to use.
   dataSource!: MatTableDataSource<any>;
 
+  // mat-paginator options.
+  pageSizeOptions = [10, 25, 50, 100];
+  showFirstLastButtons = true;
+
+  // PageRequest default options.
+  pageRequest: PageRequest = {
+    pageIndex: 0,
+    pageSize: 25
+  };
+
   constructor(
     private checkoutService: CheckoutService,
   ) {
   }
 
   ngOnInit(): void {
-    // TODO this observable should emit books taking into consideration pagination, sorting and filtering options.
     this.checkouts$ = this.checkoutService.getCheckOuts({});
 
     // Converts a page of checkouts into a suitable array for mat-table to use as a dataSource.
@@ -49,5 +60,30 @@ export class CheckoutsTableComponent implements OnInit {
     });
 
   }
+
+  // Handles the events of the user changing pageSize and pageIndex, then showing the updated mat-table.
+    handlePageEvent(event: PageEvent) {
+      this.pageRequest.pageIndex = event.pageIndex;
+      this.pageRequest.pageSize = event.pageSize;
+
+      this.checkouts$ = this.checkoutService.getCheckOuts(this.pageRequest);
+
+      this.checkouts$.subscribe(page => this.dataSource.data = page.content);
+    }
+
+    // Sorts data based on the active column and sort direction.
+    sortData(event: any) {
+      this.pageRequest.sort = event.active;
+      this.pageRequest.direction = event.direction;
+
+      // Check if the active column is borrowedBook, because initially it is an object.
+      if (event.active === 'borrowedBook') {
+        this.pageRequest.sort = 'borrowedBook.title';
+      }
+
+      this.checkouts$ = this.checkoutService.getCheckOuts(this.pageRequest);
+
+      this.checkouts$.subscribe(page => this.dataSource.data = page.content);
+    }
 
 }
